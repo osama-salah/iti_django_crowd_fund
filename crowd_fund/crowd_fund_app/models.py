98868ae867edccca7f6c3c52d45110ead32930f6
Osama Salaheldin <os.dexterity@ymail.com>
2022-10-07 10:08:42 +0200

    Initial commit

--- crowd_fund/crowd_fund_app/models.py
+++ crowd_fund/crowd_fund_app/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class User(models.Model):
-
-    name = models.CharField(max_length=25)
-    last_name =  models.CharField(max_length=25)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    mobile = models.CharField(max_length=11)
