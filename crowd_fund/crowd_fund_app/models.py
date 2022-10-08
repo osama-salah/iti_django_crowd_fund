d19065f9f6e5887f763adcf0b88e66d71badb532
Osama Salaheldin <os.dexterity@ymail.com>
2022-10-08 10:55:21 +0200

    added all models

--- crowd_fund/crowd_fund_app/models.py
+++ crowd_fund/crowd_fund_app/models.py
@@ -9,19 +9,53 @@ def mobile_validator(mobile):
         raise ValidationError(f'${mobile} is not a valid mobile number')
 
 
+def greater_than_zero_validator(value):
+    if value <= 0:
+        raise ValidationError(f'${value} must be greater than zero')
+
+
 class User(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, primary_key=True)
     last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
     mobile = models.CharField(max_length=11, validators=[mobile_validator])
-    picture = models.ImageField(null=True, upload_to='crowd_fund_app/static/crowd_fund_app/images/')
-    status = models.Choices([
+    picture = models.ImageField(null=True, upload_to='crowd_fund_app/media/crowd_fund_app/images/users/')
+    status = models.CharField(max_length=9, choices=[
         ('inactive', 'Inactive'),       # Inactivated by an admin or timeout
         ('pending', 'Pending'),         # Hasn't activated his account yet
         ('active', 'Active'),           # Activated
         ('suspended', 'Suspended'),     # Banned
     ])
-    # projects = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
-    # donations = models.ForeignKey(Donation, on_delete=models., null=True)
 
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=16)
+
+
+class ProjectTags(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=64, null=False)
+    details = models.TextField(max_length=1024)
+    category = models.ManyToManyField(to=ProjectCategory, related_name='projects')
+    user = models.ManyToManyField(to=User, related_name='projects')
+    total_target = models.FloatField(null=False, validators=[greater_than_zero_validator])
+    tags = models.ManyToManyField(to=ProjectTags, related_name='projects')
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+
+class Donation(models.Model):
+    user = models.OneToOneField(to=User, related_name='donations', on_delete=models.CASCADE)
+    project = models.OneToOneField(to=Project, related_name='donations', on_delete=models.CASCADE)
+    amount = models.FloatField(null=False, validators=[greater_than_zero_validator])
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Image(models.Model):
+    file = models.ImageField(null=True, upload_to='crowd_fund_app/media/crowd_fund_app/images/projects/')
+    project = models.ManyToManyField(to=Project, related_name='images')
